2c1b14efeb960371c841ab15ef7a8aecdb59fc71
Duliangheng2003 <duliangheng2003@163.com>
2024-10-28 21:30:54 +0800

    Initial commit

---
+++ hm-dianping/src/main/java/com/hmdp/config/RedissonConfig.java
@@ -0,0 +1,22 @@
+package com.hmdp.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RedissonConfig {
+
+    @Bean
+    public RedissonClient redissonClient(){
+        //配置类
+        Config config = new Config();
+        //添加redis单点地址
+        config.useSingleServer().setAddress("redis://192.168.202.128:6379").setPassword("1234");
+        //创建RedissonClient对象
+        return Redisson.create(config);
+    }
+
+}
